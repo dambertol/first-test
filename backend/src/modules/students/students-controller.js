3d29dbe39a34f58ec44545a4196f0ff8702ef1a8
damian <d.rambertol@gmail.com>
2024-11-22 20:58:44 -0300

    primer examen2

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,28 +2,44 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    try {
+       // const items = await Item.find();
+        res.status(200).json(getAllStudents(req.params));
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    try {
+         res.status(200).json(addNewStudent(req.params));
+     } catch (error) {
+         res.status(500).json({ error: error.message });
+     }
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    try {
+         res.status(200).json(updateStudent(req.params));
+     } catch (error) {
+         res.status(500).json({ error: error.message });
+     }
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    try {
+         res.status(200).json(getStudentDetail(req.params));
+     } catch (error) {
+         res.status(500).json({ error: error.message });
+     }
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    try {
+         res.status(200).json(setStudentStatus(req.params));
+     } catch (error) {
+         res.status(500).json({ error: error.message });
+     }
 });
 
 module.exports = {
